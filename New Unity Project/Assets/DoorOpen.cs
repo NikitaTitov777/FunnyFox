f4ca1b5fbefd53c88e6ca51e9ce0aa6ce6761aed
NikitaTitov777 <54479740+NikitaTitov777@users.noreply.github.com>
2019-08-24 19:48:27 +0300

    First commit

---
+++ New Unity Project/Assets/DoorOpen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+
+    private Animator doorAnim;
+
+    void Start()
+    {
+        doorAnim = gameObject.GetComponent<Animator>();//подключаемся к компоненту Animator 
+    }
+
+    public void Open()
+    {//меняем значение переменной на ParOpen на true 
+        doorAnim.SetBool("ParOpen", true);
+    }
+
+    public void Close()
+    {//меняем значение переменной на ParOpen на false 
+        doorAnim.SetBool("ParOpen", false);
+
+    }
+}
---
+++ New Unity Project/Assets/FPS.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS : MonoBehaviour
+{
+    public float speed = 0.25f; // переменная скорости 
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed; //    Horizontal - дополнительное имя для сопоставления с клавиатурой 
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        transform.Translate(deltaX, 0, deltaZ);//перемещение объекта в плоскости X0Z 
+    }
+
+}
